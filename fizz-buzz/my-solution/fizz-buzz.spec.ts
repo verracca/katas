ab96287a4e744a9cdf9eccdfdb9cc0607781bca7
Maria Veronica Racca <veronica.racca@gmail.com>
2020-11-23 12:45:13 +0100

    Kata completed with tests

--- fizz-buzz/my-solution/fizz-buzz.spec.ts
+++ fizz-buzz/my-solution/fizz-buzz.spec.ts
@@ -15,3 +15,35 @@ describe('doFizzBuzz', () => {
     expect(actual.length).toBe(5)
   })
 })
+
+describe('doFizzBuzz', () => {
+  it('should replace with fizz the num divisible by 3 and buzz the num divisible by 5', () => {
+    const given = 5
+    const actual = doFizzBuzz(given)
+    expect(actual).toEqual([1, 2, 'fizz', 4, 'buzz'])
+  })
+})
+
+describe('doFizzBuzz', () => {
+  it('should replace with fizzbuzz when the num is divisible by 3 and 5', () => {
+    const given = 15
+    const actual = doFizzBuzz(given)
+    expect(actual).toEqual([
+      1,
+      2,
+      'fizz',
+      4,
+      'buzz',
+      'fizz',
+      7,
+      8,
+      'fizz',
+      'buzz',
+      11,
+      'fizz',
+      13,
+      14,
+      'fizzbuzz'
+    ])
+  })
+})
--- fizz-buzz/my-solution/fizz-buzz.ts
+++ fizz-buzz/my-solution/fizz-buzz.ts
@@ -2,7 +2,15 @@ export function doFizzBuzz(number: number) {
   const myArray = []
 
   for (let i = 1; i <= number; i++) {
-    myArray.push(i)
+    if (i % 3 === 0 && i % 5 === 0) {
+      myArray.push('fizzbuzz')
+    } else if (i % 3 === 0) {
+      myArray.push('fizz')
+    } else if (i % 5 === 0) {
+      myArray.push('buzz')
+    } else {
+      myArray.push(i)
+    }
   }
 
   return myArray
